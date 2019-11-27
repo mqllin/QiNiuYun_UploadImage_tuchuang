07866508e88f9d23806014d043dcfa2b5645330a
yulin <584363361@qq.com>
2019-11-27 16:25:12 +0800

    Update config.php

--- server/config.php
+++ server/config.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: xubowen
- * Date: 2017/01/05
+ * User: 马青林
+ * Date: 2019/11/27
  * Time: 14:33
  */
   //配置文件
-  define('AK','_yzSQ5c6yh50GnIHE6D-6r0ZWAqeUiV7WpWmBCmv');//七牛云accesskey
-  define('SK','cHTpWMlfo5CCk_fyPiq5jcBuOBMSmre3P8W0Invp');//七牛云secretkey
-  define('BUCKET','yltuchuang');//七牛云空间
-  define('DOMAIN','qny.mqllin.cn');//七牛云绑定域名
+  define('AK','accesskey');//七牛云accesskey
+  define('SK','secretkey');//七牛云secretkey
+  define('BUCKET','空间名');//七牛云存储空间名
+  define('DOMAIN','www.baidu.com');//七牛云绑定域名 如www.baidu.com
